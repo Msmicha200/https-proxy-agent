475d386e59f4ec037600e198397a2309c5a23e73
Mykhailo Kirovych <msmicha200@gmail.com>
2023-07-13 23:00:32 -0400

    Fixed package.json

--- dist/parse-proxy-response.js
+++ dist/parse-proxy-response.js
@@ -1,18 +1,21 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.parseProxyResponse = void 0;
-var debug_1 = require("debug");
-var debug = (0, debug_1.default)('https-proxy-agent:parse-proxy-response');
+const debug_1 = __importDefault(require("debug"));
+const debug = (0, debug_1.default)('https-proxy-agent:parse-proxy-response');
 function parseProxyResponse(socket) {
-    return new Promise(function (resolve, reject) {
+    return new Promise((resolve, reject) => {
         // we need to buffer any HTTP traffic that happens with the proxy before we get
         // the CONNECT response, so that if the response is anything other than an "200"
         // response code, then we can re-play the "data" events on the socket once the
         // HTTP parser is hooked up...
-        var buffersLength = 0;
-        var buffers = [];
+        let buffersLength = 0;
+        const buffers = [];
         function read() {
-            var b = socket.read();
+            const b = socket.read();
             if (b)
                 ondata(b);
             else
@@ -36,36 +39,35 @@ function parseProxyResponse(socket) {
         function ondata(b) {
             buffers.push(b);
             buffersLength += b.length;
-            var buffered = Buffer.concat(buffers, buffersLength);
-            var endOfHeaders = buffered.indexOf('\r\n\r\n');
+            const buffered = Buffer.concat(buffers, buffersLength);
+            const endOfHeaders = buffered.indexOf('\r\n\r\n');
             if (endOfHeaders === -1) {
                 // keep buffering
                 debug('have not received end of HTTP headers yet...');
                 read();
                 return;
             }
-            var headerParts = buffered.slice(0, endOfHeaders).toString('ascii').split('\r\n');
-            var firstLine = headerParts.shift();
+            const headerParts = buffered.slice(0, endOfHeaders).toString('ascii').split('\r\n');
+            const firstLine = headerParts.shift();
             if (!firstLine) {
                 socket.destroy();
                 return reject(new Error('No header received from proxy CONNECT response'));
             }
-            var firstLineParts = firstLine.split(' ');
-            var statusCode = +firstLineParts[1];
-            var statusText = firstLineParts.slice(2).join(' ');
-            var headers = {};
-            for (var _i = 0, headerParts_1 = headerParts; _i < headerParts_1.length; _i++) {
-                var header = headerParts_1[_i];
+            const firstLineParts = firstLine.split(' ');
+            const statusCode = +firstLineParts[1];
+            const statusText = firstLineParts.slice(2).join(' ');
+            const headers = {};
+            for (const header of headerParts) {
                 if (!header)
                     continue;
-                var firstColon = header.indexOf(':');
+                const firstColon = header.indexOf(':');
                 if (firstColon === -1) {
                     socket.destroy();
-                    return reject(new Error("Invalid header from proxy CONNECT response: \"".concat(header, "\"")));
+                    return reject(new Error(`Invalid header from proxy CONNECT response: "${header}"`));
                 }
-                var key = header.slice(0, firstColon).toLowerCase();
-                var value = header.slice(firstColon + 1).trimStart();
-                var current = headers[key];
+                const key = header.slice(0, firstColon).toLowerCase();
+                const value = header.slice(firstColon + 1).trimStart();
+                const current = headers[key];
                 if (typeof current === 'string') {
                     headers[key] = [current, value];
                 }
@@ -80,11 +82,11 @@ function parseProxyResponse(socket) {
             cleanup();
             resolve({
                 connect: {
-                    statusCode: statusCode,
-                    statusText: statusText,
-                    headers: headers,
+                    statusCode,
+                    statusText,
+                    headers,
                 },
-                buffered: buffered,
+                buffered,
             });
         }
         socket.on('error', onerror);
@@ -93,3 +95,4 @@ function parseProxyResponse(socket) {
     });
 }
 exports.parseProxyResponse = parseProxyResponse;
+//# sourceMappingURL=parse-proxy-response.js.map
